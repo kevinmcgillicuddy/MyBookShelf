476c80b7e6995984fc4d57994f9cb9937b068e47
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2021-07-24 20:29:40 -0400

    working non optimized

--- src/store/index.js
+++ src/store/index.js
@@ -6,78 +6,48 @@ Vue.use(Vuex);
 let books = [];
 
 let years = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021];
-let categories = [1,2,3];
+let categories = [1, 2, 3];
 
 let chartDataPagesPerYear = [];
 let chartDataBooksPerYear = [];
 
 
-async function getAllBooks(){
- await  firestore.collection('Bookshelf').get()
-  .then(snapshot => {
-    snapshot.forEach(book => {
-      let b = book.data()
-      b.id = book.id
-      books.push(b)
-    });
-  })
+async function getAllBooks() {
+  await firestore.collection('Bookshelf').get()
+    .then(snapshot => {
+      snapshot.forEach(book => {
+        let b = book.data()
+        b.id = book.id
+        books.push(b)
+      });
+    })
 }
-
-// async function getPagesPerYear(year){
-//   let pagesRead = 0;
-//   for (const book of books){
-//     console.log(book.pages)
-//     if (book.date_read == year){
-//       pagesRead += parseInt(book.pages)
-//     }
-//     return pagesRead;
-//   }
-// }
-
-// async function getBooksPerYear(year){
-//   let booksRead = 0;
-//   for (const book of books){
-//     if (book.year == year){
-//       booksRead += parseInt(book.id)
-//     }
-//     return booksRead;
-//   }
-// }
-
 
 async function populateData(years) {
 
   await getAllBooks();
-  let pagesRead = 0;
-  let booksRead = 0;
-  const addArrayItems = (accumulator, currentValue) => accumulator + currentValue;
+  
+  
+
+
+
+  const addArrayItems = (total, amount) => total + amount;
 
   for (const year of years) {
-    let BooksPerYearResponse = books.filter(book => book.year_read == parseInt(year));
-    // console.log(BooksPerYearResponse)
+    let BooksPerYearResponse = books.filter(book => parseInt(book.year_read) == year);
     chartDataBooksPerYear.push(BooksPerYearResponse.length)
 
 
-    let result = books.flatMap(a => parseInt(a.year_read));
-    console.log(result)
-    let PagesPerYearResponse = books.filter(book => book.year_read == parseInt(year));
-    // console.log(PagesPerYearResponse)
-    // console.log(PagesPerYearResponse.reduce(addArrayItems))
-    chartDataPagesPerYear.push(PagesPerYearResponse.reduce(addArrayItems))
-
-    // console.log(chartDataPagesPerYear)
-    // PagesPerYearResponse = books.filter(book=>)
-    // let PagesPerYearResponse = await getPagesPerYear(parseInt(year))
-    // chartDataPagesPerYear.push(PagesPerYearResponse)
-    // let BooksPerYearResponse = await getBooksPerYear(parseInt(year))
-    // chartDataBooksPerYear.push(BooksPerYearResponse)
+    let PagesPerYearFilter = books.filter(book => parseInt(book.year_read) == year)
+    let PagesPerYearMap = PagesPerYearFilter.map(book => parseInt(book.pages))
+    chartDataPagesPerYear.push(PagesPerYearMap.reduce(addArrayItems))
   }
+
+  // books.forEach(book => yearToPageMap.set(book.year, yearToPageMap.has(book.year) ? yearToPageMap.get(book.year) + book.pages : book.pages));
+
 }
 
-populateData(years).then(d=>console.log(d))
-// getAllBooks()
-
-
+populateData(years).then(d => console.log(d))
 
 export default new Vuex.Store({
   state: {
