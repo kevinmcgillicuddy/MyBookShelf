4a9e6aa9fd66c97e16620c579f0f5921fb952526
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2023-02-18 09:06:26 -0500

    add 2023

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import firestore from '@/firebase/init';
 
 Vue.use(Vuex);
 let books = [];
-let years = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
+let years = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023];
 let chartDataPagesPerYear = [];
 let chartDataBooksPerYear = [];
 
