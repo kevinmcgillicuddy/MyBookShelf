3650328f4e9d0d0a74891ff1413bd1c250fe6784
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2023-04-07 14:43:15 -0400

    rework to use service

--- src/app/book-view/book-view.component.ts
+++ src/app/book-view/book-view.component.ts
@@ -1,169 +1,17 @@
-import { Component, OnInit,inject } from '@angular/core';
-import { BehaviorSubject, firstValueFrom, forkJoin, Observable, scan, take, tap  } from 'rxjs';
-import { BookData } from '../models/bookData';
-import { collection, query, orderBy, startAfter, limit, getDocs, DocumentData, Query, QueryDocumentSnapshot, where } from "firebase/firestore";
-import { FirebaseApp } from '@angular/fire/compat';
-import { HttpClient } from '@angular/common/http';
-import { BookImg } from '../models/imgData';
-import { Firestore } from 'firebase/firestore';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Component } from '@angular/core';
 import { AngularFireService } from 'src/services/angular-fire.service';
 
 @Component({
   selector: 'app-book-view',
   templateUrl: './book-view.component.html',
 })
-export class BookViewComponent implements OnInit {
+export class BookViewComponent{
 
-  constructor(private http: HttpClient,
-    private angularFireService: AngularFireService, private afs: AngularFirestore, private readonly firebaseApp: FirebaseApp) {
+  constructor(private angularFireService: AngularFireService) { }
 
-  }
-
-  public booksData$ = new BehaviorSubject<{
-    isProcessing?: boolean;
-    result?: BookData[];
-    error?: any;
-    finished?:boolean;
-    cursor?: QueryDocumentSnapshot<DocumentData>;
-  }>({});
-
-  private lastVisible?: QueryDocumentSnapshot<DocumentData>;
-  private next?: Query<DocumentData>;
-
-  //new stuff
-    // Observable data
-  // Source data
-  private _done = new BehaviorSubject(false);
-  private _loading = new BehaviorSubject(false);
-  private _data = new BehaviorSubject<any>([]);
-
-   // Observable data
-  data?: Observable<any>;
-  done: Observable<boolean> = this._done.asObservable();
-  loading: Observable<boolean> = this._loading.asObservable();
-
-    //end new stuff
-
-  async ngOnInit(): Promise<void> {
-    this.angularFireService.getInitBookView();
-
-      const first = this.afs.collection<BookData>('Bookshelf', ref => {
-        return ref
-                .orderBy('year_read', 'desc' )
-                .limit(8)
-      })
-
-      this.mapAndUpdate2(first)
-      // Create the observable array for consumption in components
-      this.data = this._data.asObservable().pipe(
-        scan( (acc, val) => {
-          return acc.concat(val)
-        })
-      )
-
-    }
-
-    more() {
-      const cursor = this.getCursor()
-      const more = this.afs.collection('Bookshelf', ref => {
-        return ref
-                .orderBy('year_read', 'desc' )
-                .limit(8)
-                .startAfter(cursor)
-      })
-      this.mapAndUpdate2(more)
-    }
-
-  // Maps the snapshot to usable format the updates source
-  private mapAndUpdate2(col: AngularFirestoreCollection<any>) {
-
-    if (this._done.getValue() || this._loading.getValue()) { return };
-
-    // loading
-    this._loading.next(true)
-
-    // Map snapshot with doc ref (needed for cursor)
-    return col.snapshotChanges()
-      .pipe(
-        tap((arr) => {
-        let values = arr.map(snap => {
-          const data = snap.payload.doc.data()
-          const doc = snap.payload.doc
-          return { ...data, doc }
-        })
-
-        // If prepending, reverse the batch order
-        values = values
-
-        // update source with new values, done loading
-        this._data.next(values)
-        this._loading.next(false)
-
-        // no more values, mark done
-        if (!values.length) {
-          this._done.next(true)
-        }
-    },
-    take(1))
-    )
-    .subscribe(console.log)
-
-  }
-
-      // Determines the doc snapshot to paginate query
-  private getCursor() {
-    const current = this._data.getValue()
-    if (current.length) {
-      return current[current.length - 1].doc
-    }
-    return null
-  }
-
+  public booksData$ = this.angularFireService.bookData$;
 
   async onScroll(): Promise<void> {
     this.angularFireService.getMore();
-    this.more();
-    // this.afs.collection<BookData>('Bookshelf',ref => ref.startAfter()).get().subscribe((data) => {console.log(data)})
-    // Set the isProcessing flag to true
-    this.booksData$.next({ ...this.booksData$.value, isProcessing: true });
-
-    // Query the next page of docs and map the data
-    const data = await this.getDataAndMap(this.next!);
-    // Set the data to the booksData$ BehaviorSubject
-    this.booksData$.next({ result: this.booksData$.value?.result?.concat(data), isProcessing: false });
   }
-
-
-
-
-  async getDataAndMap(searchQuery?: Query<DocumentData>): Promise<BookData[]> {
-    // query the supplied query or the default query
-    const data = await getDocs<DocumentData>(searchQuery || query(collection(this.firebaseApp.firestore(), "Bookshelf"), orderBy("year_read", 'desc'), limit(8)));
-    // set the next query for next onScroll
-    this.prepNext(data.docs[data.docs.length - 1]);
-    // get the image data from google apis
-    const imgData = await firstValueFrom(forkJoin(data.docs.map((d) => this.getImgs(d.data()['isbn']))));
-    // map the data and return
-    const result = data.docs.map((d, i) => ({
-      ...d.data(),
-      img_url: imgData![i].items[0]?.volumeInfo?.imageLinks?.smallThumbnail || 'https://dummyimage.com/720x400&text=%20'
-    } as BookData));
-    return result;
-  }
-
-
-  prepNext(nextVisible: QueryDocumentSnapshot<DocumentData> ): void {
-    //this is the function that sets the next query based on the last visible document
-    this.lastVisible = nextVisible;
-    this.next = query(collection(this.firebaseApp.firestore(), "Bookshelf"),
-      orderBy("year_read", 'desc'),
-      startAfter(this.lastVisible),
-      limit(6));
-  }
-
-  getImgs(isbn: string): Observable<BookImg> {
-    return this.http.get<BookImg>(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`)
-  }
-
 }
--- src/services/angular-fire.service.ts
+++ src/services/angular-fire.service.ts
@@ -1,22 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, DocumentData, QueryDocumentSnapshot } from '@angular/fire/compat/firestore';
-import { BehaviorSubject, firstValueFrom, forkJoin, map, Observable, scan, take, tap } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection, DocumentChangeAction, DocumentData, QueryDocumentSnapshot } from '@angular/fire/compat/firestore';
+import { BehaviorSubject, firstValueFrom, forkJoin, map, Observable, OperatorFunction, scan, switchMap, take, tap } from 'rxjs';
 import { BookData } from 'src/app/models/bookData';
+import { BookImg } from 'src/app/models/imgData';
 @Injectable({
   providedIn: 'root'
 })
 export class AngularFireService {
-
-  private _done = new BehaviorSubject(false);
-  private _loading = new BehaviorSubject(false);
-  private _data = new BehaviorSubject<any>([]);
-
-  // Observable data
-  data$?: Observable<BookData[] | undefined>;
-  done: Observable<boolean> = this._done.asObservable();
-  loading: Observable<boolean> = this._loading.asObservable();
-
 
   private _booksData$ = new BehaviorSubject<{
     isProcessing?: boolean;
@@ -24,59 +15,94 @@ export class AngularFireService {
     error?: any;
     finished?: boolean;
     cursor?: any;
-  }>({});
+  }>({
+    result: [],
+  });
 
+  public bookData$ = this._booksData$.asObservable();
 
-  constructor(private http: HttpClient, private afs: AngularFirestore) { }
-
-  generateCollection(cursor?: QueryDocumentSnapshot<DocumentData>) {
-    return this.afs.collection<BookData>('Bookshelf', ref => {
-      return cursor ?  ref.orderBy('year_read', 'desc').limit(8).startAfter(cursor) : ref.orderBy('year_read', 'desc').limit(8)
-    })
-  }
-
-  //move this to the constructor?
-  public getInitBookView() {
+  constructor(private http: HttpClient, private afs: AngularFirestore) {
+    //set the initial value when the service is called
     this.mapAndUpdateBookData(this.generateCollection())
-    // Create the observable array for consumption in components
-    // this.data$ = this._booksData$.asObservable().pipe(
-    //   map(({ result })=> result),
-    //   scan((acc, val) => {
-    //     return acc.concat(val)
-    //   })
-    // )
   }
 
-  public getMore(){
-    console.log('getMore')
-    this._booksData$.next({ ...this._booksData$.value, cursor: this._booksData$.value.result?.length || null });
+  /**
+* This method is called by the implementing component to load more books into the behaviorSubject
+*  */
+  public getMore() {
     this.mapAndUpdateBookData(this.generateCollection(this._booksData$.value.cursor))
   }
 
-  private mapAndUpdateBookData(col: AngularFirestoreCollection<BookData>): void {
+  /**
+ * This method generates a Firestore collection reference for the 'Bookshelf' collection.
+ * The method accepts an optional QueryDocumentSnapshot object called 'cursor' which is used to specify
+ * the starting point for the query. If no cursor is provided, the query starts from the beginning of the collection.
+ *  */
+  private generateCollection(cursor?: QueryDocumentSnapshot<DocumentData>) {
+    return this.afs.collection<BookData>('Bookshelf', ref => {
+      return cursor ? ref.orderBy('year_read', 'desc').startAfter(cursor).limit(8) : ref.orderBy('year_read', 'desc').limit(8)
+    })
+  }
 
+  /**
+ * This method maps and updates book data from a given AngularFirestoreCollection.
+ * It first checks if the data is already finished or is currently processing, and if so, it returns immediately.
+ * If the data is not finished or processing, it sets the 'isProcessing' property of the '_booksData$' BehaviorSubject to true.
+ *
+ * The method then retrieves the snapshot changes of the collection and pipes them through a series of operators:
+ *  - The 'tap' operator checks if the length of the array of changes is zero and, if so, sets the 'finished' property of the '_booksData$'
+ *    BehaviorSubject to true.
+ *  - The 'switchMap' operator maps the array of changes to an array of BookData objects, and then maps each BookData object to an Observable
+ *    of BookImg objects using the 'getImgs' method. These Observables are then combined into a single Observable using 'forkJoin'.
+ *    Once the images are retrieved, the operator maps the original array of BookData objects to a new array of objects that includes the
+ *    URL of the book cover image.
+ *  - The 'take' operator ensures that the Observable completes after emitting a single value.
+ *
+ * The method then subscribes to the Observable and updates the '_booksData$' BehaviorSubject with the new data. The 'result' property
+ * of the BehaviorSubject is updated by concatenating the current result with the new result.
+ */
+  private mapAndUpdateBookData(col: AngularFirestoreCollection<BookData>): void {
     if (this._booksData$.getValue().finished || this._booksData$.getValue().isProcessing) return;
-    // loading
+
+    // Set isProcessing to true to indicate that the data is currently being processed.
     this._booksData$.next({ ...this._booksData$.value, isProcessing: true });
 
-    // Map snapshot with doc ref (needed for cursor)
-     col.valueChanges()
+    // Get the snapshot changes of the collection and pipe them through a series of operators.
+    col.snapshotChanges()
       .pipe(
-        map((arr) => {
-          let values = arr.map(snap =>snap as BookData)
+        // Check if the length of the array of changes is zero and, if so, set 'finished' to true.
+        tap((arr) => { if (arr.length === 0) this._booksData$.next({ ...this._booksData$.value, finished: true }) }),
 
-          // update source with new values, done loading
-          this._data.next(values)
-          this._loading.next(false)
+        // Map the array of changes to an array of BookData objects and get the book cover images.
+        switchMap((arr: DocumentChangeAction<BookData>[]) => {
+          // Set the 'cursor' property of the '_booksData$' BehaviorSubject to the last document in the array of changes.
+          this._booksData$.next({ ...this._booksData$.value, cursor: arr[arr.length - 1].payload.doc })
 
-          // no more values, mark done
-          if (!values.length) {
-            this._booksData$.next({...this._booksData$.value, finished: true})
-          }
-        },
-        take(1))
+          // Map the array of changes to an array of BookData objects.
+          const values = arr.map(snap => snap.payload.doc.data() as BookData)
+
+          // Map each BookData object to an Observable of BookImg objects using the 'getImgs' method.
+          const imgObservables = values.map(book => this.getImgs(book.isbn));
+
+          // Combine the Observables of BookImg objects into a single Observable using 'forkJoin'.
+          return forkJoin(imgObservables).pipe(
+            // Map the original array of BookData objects to a new array that includes the URL of the book cover image.
+            map((imgs: BookImg[]) => {
+              return values.map((book, i) => {
+                return { ...book, img_url: imgs[i].items[0].volumeInfo.imageLinks.thumbnail };
+              });
+            })
+          );
+        }),
+        // Ensure that the Observable completes after emitting a single value.
+        take(1)
       )
-      .subscribe((d)=>{console.log('d: ', d)})
-
+      .subscribe((result) => this._booksData$.next({ ...this._booksData$.value, result: this._booksData$.value.result?.concat(...result), isProcessing: false }))
   }
+
+  //takes an isbn number and returns an observable of BookImg from the Google Books API
+  getImgs(isbn: string): Observable<BookImg> {
+    return this.http.get<BookImg>(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`)
+  }
+
 }
